54672870d34300095f6a56d322a14d7a16a939a5
prince475 <danielmwai475@gmail.com>
2022-12-12 15:27:37 +0300

    solution 2 to the palindrome problem using iteration

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning to the middle
+  for(let i=0; i < word.length / 2; i++){
+     //   check each leteer to the corresponding letter from the end
+     const j = word.length - 1 - i;
+     const startChar = word[i];
+     const endChar = word[j];
+  //     if any letters don't match, return false.
+
+      if (startChar !== endChar) return false;
+
+
+  }
+  
+  // return type(true, false)
+  return true;
+
+
 }
 
 /* 
+how the iteration will execute
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
+   
+
+
   Add your pseudocode here
+  that means if the first letter is the same a the last letter and the second letter
+  is the sam as the second to last letter, etc. should return true
+
+  iterate from the beginning to the middle
+    check each leteer to the corresponding letter from the end
+      if any letters don't match, return false.
+
+  otherwise return true
 */
 
 /*
@@ -20,6 +64,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("oppo"))
 }
 
 module.exports = isPalindrome;
